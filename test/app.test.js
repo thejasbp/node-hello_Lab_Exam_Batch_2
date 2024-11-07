81f38a0873899aad8a588f5a96062e6f968967f6
root <thejasbp2002@gmail.com>
2024-11-07 18:51:14 +0530

    The test files are added

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const app = require('../app');  // Assuming app.js is the entry point of the app
+
+describe('App Tests', function () {
+  it('should return 200 for the home route', function (done) {
+    app.listen(3000, () => {
+      request('http://localhost:3000', function (error, response, body) {
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
+});
